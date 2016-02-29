47ee3df706704cf1a934e216f47db32e5c95d6cf
zhou lin <boyzhoulin@sina.com>
2016-02-29 22:58:29 +0800

    send response based on db

--- server/Startup.js
+++ server/Startup.js
@@ -7,6 +7,21 @@ Meteor.startup(function () {
 	res.setHeader("Access-Control-Allow-Methods","*");
 	res.setHeader("Access-Control-Allow-Headers","Content-Type");
     res.setHeader('Strict-Transport-Security', 'max-age=2592000; includeSubDomains'); // 2592000s / 30 days
-    return next();
+	
+	console.log(req.method +  '       ' + req.url)
+	var resObj = getRequest(req.method,req.url)
+	console.log(resObj)
+	if(resObj){
+		Meteor.setTimeout(function(){
+			res.end(JSON.stringify(resObj.response))
+		},parseInt(resObj.timeout))
+	}else {
+    	return next();
+	}	
   })
 })
+
+function getRequest(method,api) {
+	var item = RoutesDB.findOne({method:method.toLowerCase(),api:api.toLowerCase()})
+	return item
+}
